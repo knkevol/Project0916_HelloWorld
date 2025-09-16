149f68c0b261488b10def2650522eb1bd86d5e21
jungsa <106296196+knkevol@users.noreply.github.com>
2025-09-16 10:44:54 +0900

    Attack 및 Heal

--- main.cpp
+++ main.cpp
@@ -4,6 +4,16 @@ using namespace std;
 
 int main()
 {
+	int PlayerHP = 100;
+	int Attack = 20;
+	int CurrentHP = 0;
 
+	CurrentHP = PlayerHP - Attack;
+	
+	int Heal = CurrentHP + 50;
+
+	CurrentHP = Heal;
+
+	cout << CurrentHP;
 	return 0;
 }
