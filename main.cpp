9f69ae61c90d2839582da4a72ec004b9abeb1c64
jungsa <106296196+knkevol@users.noreply.github.com>
2025-09-16 13:46:11 +0900

    이동로직 작성

--- main.cpp
+++ main.cpp
@@ -4,58 +4,45 @@
 
 using namespace std;
 
+//entry point
 int main()
 {
-	// 방향이동
 	int PlayerX = 0;
 	int PlayerY = 0;
-	char PlayerShape = '@';
+	bool bIsRunning = true;
 
-	// 상태변수
-	bool IsAlive = true;
-	bool IsHungry = true;
-
-	// 처리한다
-	// Process
-	PlayerX++;
-	PlayerY++;
-
-	// 엔진
-	// Render(); | 1frame
-	COORD Cur; 
-	Cur.X = PlayerX;
-	Cur.Y = PlayerY;
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
-	cout << PlayerShape << endl;
-	_getch(); // 이전 위치 제거
-	system("cls");
-
-	PlayerX++; // input
-	Cur.X = PlayerX;
-	Cur.Y = PlayerY;
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
-	cout << PlayerShape << endl;
-	_getch();
-	system("cls");
-
-	PlayerY++;
-	Cur.X = PlayerX;
-	Cur.Y = PlayerY;
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
-	cout << PlayerShape << endl;
-	_getch();
-	system("cls");
-
-
-	/*
-	* GameLoop *
-	while (true)
+	while (bIsRunning)
 	{
-	Input
-	Process
-	Render
+		int KeyCode = _getch(); // 입력 받는 것을 출력
+		if (KeyCode == 'w')
+		{
+			PlayerY--;
+		}
+		else if (KeyCode == 's')
+		{
+			PlayerY++;
+		}
+		else if (KeyCode == 'a')
+		{
+			PlayerX--;
+		}
+		else if (KeyCode == 'd')
+		{
+			PlayerX++;
+		}
+		else if (KeyCode == 'q')
+		{
+			bIsRunning = false;
+		}
+		system("cls");
+
+		COORD Cur;
+		Cur.X = PlayerX;
+		Cur.Y = PlayerY;
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
+
+		cout << "P" << endl;
 	}
-	*/
 
 	return 0;
 }
